f1853d7cbb1876c830b4a1188e6a214e7c675a47
sdey <saswata.dey@moesol.com>
2016-03-25 14:15:00 -0700

    First commit

---
+++ env_tut/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sdey\documents\moebius\portal\tutorial\env_tut\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
